8552afc8ccc07be2f64b9a3503b0872a50d6c8b4
YX_DD\yangx <yangxiaozlp@gmail.com>
2018-05-27 15:00:43 +0800

    dd

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hellotest {name}!</h3>" \
+    html = "<h3>Hello {name}!</h3>" \
+		   "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
